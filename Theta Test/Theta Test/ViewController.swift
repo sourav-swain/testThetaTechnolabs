a57844a3ca757bd5e092779cacf861d3a88c80dc
Sourav Swain <sourav.tinu@hotmail.com>
2022-01-31 20:01:54 +0530

    Added starting project

---
+++ Theta Test/Theta Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Theta Test
+//
+//  Created by Sourav Swain on 31/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
